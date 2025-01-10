0ef6cb0324a46e1c7ffc30a63518c43157304df0
almend-2000 <plabvosguzmanverrotern@gmail.com>
2025-01-09 22:13:25 -0400

    Added StringDuplicateRemover test cases

--- src/test/java/com/softserve/duplicateremover/StringDuplicateRemoverTest.java
+++ src/test/java/com/softserve/duplicateremover/StringDuplicateRemoverTest.java
@@ -7,13 +7,39 @@ import org.junit.jupiter.api.Test;
 
 class StringDuplicateRemoverTest {
 
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-	}
-
 	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+    public void testWithAlphanumericDuplicates() {
+        assertEquals("ABCD123", StringDuplicateRemover.removeDuplicates("AABBCCD112233"));
+    }
+
+    @Test
+    public void testWithMixedCaseCharacters() {
+        assertEquals("aABbCc", StringDuplicateRemover.removeDuplicates("aAaBbBbCcC"));
+    }
+
+    @Test
+    public void testWithSpecialCharacters() {
+        assertEquals("a1!@", StringDuplicateRemover.removeDuplicates("a1!!@@1a"));
+    }
+
+    @Test
+    public void testEmptyString() {
+        assertEquals("", StringDuplicateRemover.removeDuplicates(""));
+    }
+
+    @Test
+    public void testNullInput() {
+        assertNull(StringDuplicateRemover.removeDuplicates(null));
+    }
+
+    @Test
+    public void testSingleCharacterRepeated() {
+        assertEquals("A", StringDuplicateRemover.removeDuplicates("AAAAAA"));
+    }
+
+    @Test
+    public void testAllUniqueCharacters() {
+        assertEquals("abcdef", StringDuplicateRemover.removeDuplicates("abcdef"));
+    }
 
 }
